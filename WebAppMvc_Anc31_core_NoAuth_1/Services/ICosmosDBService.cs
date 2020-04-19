cb22f078d199aeabf23202c32a6c53e398655169
Bala Chirtsabesan <balaTest@live.com>
2020-04-19 13:53:08 -0700

    Wire up connection to cosmos db

---
+++ WebAppMvc_Anc31_core_NoAuth_1/Services/ICosmosDBService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMvc_Anc31_core_NoAuth_1.Services
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using WebAppMvc_Anc31_core_NoAuth_1.Models;
+
+    public interface ICosmosDbService
+    {
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+        Task<Item> GetItemAsync(string id);
+        Task AddItemAsync(Item item);
+        Task UpdateItemAsync(string id, Item item);
+        Task DeleteItemAsync(string id);
+    }
+}
--- WebAppMvc_Anc31_core_NoAuth_1/Startup.cs
+++ WebAppMvc_Anc31_core_NoAuth_1/Startup.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebAppMvc_Anc31_core_NoAuth_1.Services;
 
 namespace WebAppMvc_Anc31_core_NoAuth_1
 {
@@ -23,9 +27,18 @@ namespace WebAppMvc_Anc31_core_NoAuth_1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-        }
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
 
+
+            services.AddControllersWithViews();
+            services.AddSingleton<ICosmosDbService>(InitializeCosmosClientInstanceAsync(Configuration.GetSection("CosmosDb")).GetAwaiter().GetResult());
+        }
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -52,6 +65,27 @@ namespace WebAppMvc_Anc31_core_NoAuth_1
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        /// <summary>
+        /// Creates a Cosmos DB database and a container with the specified partition key. 
+        /// </summary>
+        /// <returns></returns>
+        private static async Task<CosmosDbService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            string databaseName = configurationSection.GetSection("DatabaseName").Value;
+            string containerName = configurationSection.GetSection("ContainerName").Value;
+            string account = configurationSection.GetSection("Account").Value;
+            string key = configurationSection.GetSection("Key").Value;
+            CosmosClientBuilder clientBuilder = new CosmosClientBuilder(account, key);
+            CosmosClient client = clientBuilder
+                                .WithConnectionModeDirect()
+                                .Build();
+            CosmosDbService cosmosDbService = new CosmosDbService(client, databaseName, containerName);
+            DatabaseResponse database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/id");
+
+            return cosmosDbService;
         }
     }
 }
